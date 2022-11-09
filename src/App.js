7a771c760c8dee832b99782d09c7b1554158d6d1
Divyang Sharma <divyang9575@gmail.com>
2022-11-09 15:06:25 +0530

    added alerts

--- src/App.js
+++ src/App.js
@@ -3,28 +3,42 @@ import TextForms from "./components/TextForms";
 import React, {useState} from 'react';
 
 import "./App.css";
+import Alert from "./components/Alert";
 
 
 function App() {
 
   const [mode , setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg : message,
+      type : type //both names can be same
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
   
-  const toggleMode = () =>{
-    if(mode === 'light'){
+  const toggleMode = () =>{ 
+    if(mode === 'light'){ 
       setMode('dark');
       document.body.style.backgroundColor = '#181151';
+      showAlert("Dark mode has been enabled","success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-
+      showAlert("Light mode has been enabled","success");
     }
   }
   return (
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert = {alert}/>
       <div className="container my-4">
-        <TextForms text="Enter Text " mode={mode} toggleMode={toggleMode} />
+        <TextForms text="Enter Text " mode={mode} toggleMode={toggleMode} showAlert={showAlert} />
       </div>
     </>
   );
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -3,27 +3,46 @@ import PropTypes from "prop-types";
 
 export default function TextForms(props) {
   const [text, setText] = useState("");
+  
+
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     console.log("handleclick func");
     setText(newText);
+    // setAlert("")
+    props.showAlert("Converted to uppercase!","success");
   };
   const handleOnChange = (event) => {
     console.log("handleOnChange func");
     setText(event.target.value);
   };
-  const handleClear = ()=>{
-    let newText = '';
+  const handleClear = () => {
+    let newText = "";
     setText(newText);
-  }
+    props.showAlert("Text Cleared !","success");
+
+  };
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Text copied!","success");
+
+  };
   return (
     <>
-      <div className="container" style={{color: props.mode === 'dark'? 'white':'black'}}>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
         <div className="container">
           <h1>{props.text}</h1>
           <div className="form-group mb-3">
             <textarea
-            style={{backgroundColor: props.mode === 'dark'? 'grey':'white',color: props.mode === 'dark'? 'white':'black'}}
+              style={{
+                backgroundColor: props.mode === "dark" ? "grey" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
               className="form-control"
               id="myBox"
               value={text}
@@ -38,6 +57,9 @@ export default function TextForms(props) {
           <button className="btn btn-primary mx-2" onClick={handleClear}>
             Clear
           </button>
+          <button className="btn btn-primary mx-2" onClick={handleCopy}>
+            Copy Text
+          </button>
         </div>
         <div className="container my-4">
           <h1>Text Summary</h1>
@@ -48,7 +70,11 @@ export default function TextForms(props) {
         </div>
         <div className="container">
           <h2>Preview</h2>
-          <p>{text.length > 0 ? text:"Enter something in the above box to preview it"}</p>
+          <p>
+            {text.length > 0
+              ? text
+              : "Enter something in the above box to preview it"}
+          </p>
         </div>
       </div>
     </>
